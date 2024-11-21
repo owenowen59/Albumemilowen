6c1ef20a1f8c2f27f7dd856244cd6863cee7df6e
AGACHE <emilien62agache@gmail.com>
2024-11-21 12:09:45 +0100

    J'ai changé le chemin des fichiers CSS et j'ai commencé la page layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,22 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="{{env('APP_URL')}}/css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="{{asset('/css/layout.css')}}"/>
 </head>
 <body>
-    <nav id=menu>
-        <a href="{{ route('index') }}"> Home Page </a>
-        <a href="{{route('album')}}"> Album </a>
-        <a href="{{route('photo')}}"> Photo </a>
-        <a href="{{route('connexion')}}"> Connexion </a>
-
-    </nav>
+    <header>
+        <nav id=menu>
+            <a href="{{route('index') }}"> Home Page </a>
+            <a href="{{route('album')}}"> Album </a>
+            <a href="{{route('photo')}}"> Photo </a>
+            <a href="{{route('connexion')}}"> Connexion </a>
+        </nav>
+    </header>
+   
 
     <main>
         @yield('contenu')
     </main>
+
+    <footer></footer>
+
 </body>
 </html>
